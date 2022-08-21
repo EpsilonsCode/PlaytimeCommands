26211b1c154d622930343c73814bacd9d73a11be
PajacCode <michal.lutomirskii@gmail.com>
2022-08-21 13:02:20 +0000

    Add gradle setup and basics of the mod

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -1,70 +1,155 @@
 package com.example.examplemod;
 
+import com.google.gson.*;
 import com.mojang.logging.LogUtils;
-import net.minecraft.client.Minecraft;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.Material;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.dedicated.DedicatedServer;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.loading.FMLPaths;
+import net.minecraftforge.server.ServerLifecycleHooks;
 import org.slf4j.Logger;
+
+import java.io.*;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(ExampleMod.MODID)
 public class ExampleMod
 {
+    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+
+    private static final String CONFIG_PATH = FMLPaths.CONFIGDIR.get().toString() + "/playtime_commands_config.json";
+
+    private static final String SAVED_PATH = FMLPaths.CONFIGDIR.get().toString() + "/playtime_commands_saved.json";
+
+    private static HashMap<String, Milestone> milestoneMap = new HashMap<>();
     // Define mod id in a common place for everything to reference
     public static final String MODID = "examplemod";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
-    // Create a Deferred Register to hold Blocks which will all be registered under the "examplemod" namespace
-    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
-    // Create a Deferred Register to hold Items which will all be registered under the "examplemod" namespace
-    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
-
-    // Creates a new Block with the id "examplemod:example_block", combining the namespace and path
-    public static final RegistryObject<Block> EXAMPLE_BLOCK = BLOCKS.register("example_block", () -> new Block(BlockBehaviour.Properties.of(Material.STONE)));
-    // Creates a new BlockItem with the id "examplemod:example_block", combining the namespace and path
-    public static final RegistryObject<Item> EXAMPLE_BLOCK_ITEM = ITEMS.register("example_block", () -> new BlockItem(EXAMPLE_BLOCK.get(), new Item.Properties().tab(CreativeModeTab.TAB_BUILDING_BLOCKS)));
 
     public ExampleMod()
     {
-        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
-        // Register the commonSetup method for modloading
-        modEventBus.addListener(this::commonSetup);
 
-        // Register the Deferred Register to the mod event bus so blocks get registered
-        BLOCKS.register(modEventBus);
-        // Register the Deferred Register to the mod event bus so items get registered
-        ITEMS.register(modEventBus);
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
     }
 
-    private void commonSetup(final FMLCommonSetupEvent event)
+    @SubscribeEvent
+    public void onEntityJoin(PlayerEvent.PlayerLoggedInEvent event)
     {
-        // Some common setup code
-        LOGGER.info("HELLO FROM COMMON SETUP");
-        LOGGER.info("DIRT BLOCK >> {}", ForgeRegistries.BLOCKS.getKey(Blocks.DIRT));
+        if(event.getEntity().getLevel() instanceof ServerLevel && event.getEntity() instanceof ServerPlayer)
+        {
+            float playtimeInSeconds = 0;
+            //System.out.println(event.getEntity());
+
+            try {
+                Class.forName("org.mariadb.jdbc.Driver");
+            } catch (ClassNotFoundException e) {
+                System.out.println(e);
+            }
+            //create connection for a server installed in localhost, with a user "root" with no password
+            try (Connection conn = DriverManager.getConnection("jdbc:mariadb://localhost/player_data", "root", null)) {
+                // create a Statement
+                //System.out.println("sql2");
+                Statement stmt = conn.createStatement();
+                //execute query
+                //System.out.println("sql1");
+                ResultSet rs = stmt.executeQuery("SELECT Playtime FROM player_stats WHERE UUID = " + "'" + event.getEntity().getUUID() + "'");
+                //position result to first
+                //rs.first();
+
+                while(rs.next())
+                {
+                    playtimeInSeconds = Float.parseFloat(rs.getString(1));
+                }
+
+                //System.out.println(rs.getString(1)); //result is "Hello World!"
+                //System.out.println("sql");
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+
+            
+            event.getEntity().sendSystemMessage(Component.literal("You've been playing for: " + playtimeInSeconds / 3600 + " hours!"));
+            ArrayList<String> milestones = new ArrayList<>();
+            JsonObject json = new JsonObject();
+            JsonArray jsonArray = new JsonArray();
+            if(new File(SAVED_PATH).exists())
+            {
+                try {
+                    json = GSON.fromJson(new FileReader(SAVED_PATH), JsonObject.class);
+                    for (Map.Entry<String, JsonElement> entry : json.entrySet())
+                    {
+                        if(event.getEntity().getUUID().toString().equals(entry.getKey()))
+                        {
+                            if(entry.getValue().isJsonArray())
+                            {
+                                jsonArray = entry.getValue().getAsJsonArray();
+                                entry.getValue().getAsJsonArray().forEach(jsonElement -> {
+                                    milestones.add(jsonElement.toString());
+                                });
+                            }
+                        }
+                    }
+                } catch (FileNotFoundException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            else {
+
+            }
+
+
+            for(Map.Entry<String, Milestone> entry : milestoneMap.entrySet())
+            {
+                //System.out.println("MILESTONE@: " + entry.getKey());
+                if(!milestones.contains('\"' + entry.getKey() + '\"'))
+                {
+                    //System.out.println("test" + entry.getKey());
+                    if(entry.getValue().playtime <= playtimeInSeconds)
+                    {
+                        try {
+                            System.out.println("test2" + entry.getKey());
+                            jsonArray.add(entry.getKey());
+                            json.add(event.getEntity().getUUID().toString(), jsonArray);
+                            FileWriter writer = new FileWriter(SAVED_PATH);
+                            GSON.toJson(json, writer);
+                            writer.flush();
+                            writer.close();
+                        } catch (IOException e) {
+                            System.out.println(e);
+                        }
+                        if(ServerLifecycleHooks.getCurrentServer() instanceof DedicatedServer dedicatedServer)
+                        {
+                            for(String command : entry.getValue().commands)
+                            {
+                                command = command.replace("{username}", event.getEntity().getName().getString());
+                                StringBuffer sb = new StringBuffer(command);
+                                sb.deleteCharAt(sb.length() - 1);
+                                sb.deleteCharAt(0);
+                                dedicatedServer.runCommand(sb.toString());
+                                //System.out.println(sb.toString());
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
+
+
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
@@ -72,18 +157,35 @@ public class ExampleMod
     {
         // Do something when the server starts
         LOGGER.info("HELLO from server starting");
+        if(!new File(SAVED_PATH).exists())
+        try {
+            JsonObject jsonObject = new JsonObject();
+            FileWriter writer = new FileWriter(SAVED_PATH);
+            GSON.toJson(jsonObject, writer);
+            writer.flush();
+            writer.close();
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+
+        if(new File(CONFIG_PATH).exists())
+        try {
+            JsonObject json = GSON.fromJson(new FileReader(CONFIG_PATH), JsonObject.class);
+            for(Map.Entry<String, JsonElement> entry : json.entrySet())
+            {
+                ArrayList<String> commands = new ArrayList<>();
+                GsonHelper.getAsJsonArray(entry.getValue().getAsJsonObject(), "commands").forEach((jsonElement -> {
+                    commands.add(jsonElement.toString());
+                }));
+
+                milestoneMap.put(entry.getKey(), new Milestone(GsonHelper.getAsInt(entry.getValue().getAsJsonObject(), "playtime"), commands.toArray(new String[0])));
+            }
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
-    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
-    public static class ClientModEvents
-    {
-        @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event)
-        {
-            // Some client setup code
-            LOGGER.info("HELLO FROM CLIENT SETUP");
-            LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
-        }
-    }
+
 }
---
+++ src/main/java/com/example/examplemod/Milestone.java
@@ -0,0 +1,11 @@
+package com.example.examplemod;
+
+public class Milestone {
+    public int playtime;
+    public String[] commands;
+    public Milestone(int playtime, String[] commands)
+    {
+        this.playtime = playtime;
+        this.commands = commands;
+    }
+}
